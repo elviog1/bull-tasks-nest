da29da1bbb0f01fe42f8ae64082e6f806b2a632b
Elvio <elvio.galeano@gmail.com>
2024-04-29 09:58:37 -0300

    fix

--- src/task/dto/task.dto.ts
+++ src/task/dto/task.dto.ts
@@ -1,62 +1,72 @@
-import { Transform } from "class-transformer";
-import { IsDate, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
+import { Transform } from 'class-transformer';
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
-export enum TaskStatus{
-    Pending = 'Pending',
-    Approved = 'Approved',
-    Rejected = 'Rejected',
+export enum TaskStatus {
+  Pending = 'Pending',
+  Approved = 'Approved',
+  Rejected = 'Rejected',
 }
 
-export class CreateTaskDTO{
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(3)
-    title:string;
-    
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(10)
-    description:string;
-    
-    @IsEnum(TaskStatus)
-    @IsOptional()
-    status:TaskStatus;
-    
-    @IsDate()
-    @Transform(({ value }) => new Date(value))
-    date:Date;
-    
-    @IsString()
-    @IsNotEmpty()
-    userId:string
+export class CreateTaskDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  title: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(300)
+  description: string;
+
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status: TaskStatus;
+
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
+  date: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
 }
 
-export class UpdateTaskDTO{
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(3)
-    @IsOptional()
-    title:string;
-    
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(10)
-    @IsOptional()
-    description:string;
-    
-    @IsEnum(TaskStatus)
-    @IsOptional()
-    status:TaskStatus;
-    
-    @IsDate()
-    @Transform(({ value }) => new Date(value))
-    @IsOptional()
-    date:Date;
-    
-    @IsString()
-    @IsNotEmpty()
-    @IsOptional()
-    userId:string
+export class UpdateTaskDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(20)
+  @IsOptional()
+  title: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(10)
+  @MaxLength(300)
+  @IsOptional()
+  description: string;
+
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status: TaskStatus;
+
+  @IsDate()
+  @Transform(({ value }) => new Date(value))
+  @IsOptional()
+  date: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  userId: string;
 }
